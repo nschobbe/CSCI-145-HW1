1b26ef646f910f44a03b14843f2bddcfb733b597
Josh-Pedersen <59670614+Josh-Pedersen@users.noreply.github.com>
2020-02-18 21:28:35 -0800

    Update Main.java

--- src/java/main/Main.java
+++ src/java/main/Main.java
@@ -2,6 +2,9 @@ package src.java.main;
 
 public class Main {
     public static void main(String[] args) {
+        DungeonGame dungeonGame = new DungeonGame();
+        dungeonGame.setX(args[0]);
+        dungeonGame.setY(args[1]);
 
     }
 }
